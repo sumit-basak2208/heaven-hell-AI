3aec6e1ca4d5459b7b0a5c1eabb9058f06884440
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-09-29 14:34:34 +0530

    theme changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import logo from "../app/assets/hh-logo.png";
 
 export default function Navbar() {
   return (
-    <nav className="absolute top-0 left-0 right-0 px-3 py-2 bg-gradient-navbar z-20 text-white shadow-lg">
+    <nav className="absolute top-0 left-0 right-0 px-3 py-2 bg-black z-20 text-white shadow-lg border-b border-white">
       <div className="flex items-center gap-3">
-        <Image src={logo} width={30} height={30} alt="log" />
+        {/* <Image src={logo} width={30} height={30} alt="log" /> */}
         <h1 className="text-xl font-semibold mb-1">Hell Heaven AI</h1>
       </div>
     </nav>

50960fc9f91b1bd0a1b3ee6fd6b9e5b22b2e40c2
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-09-30 19:58:04 +0530

    designed message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,14 +1,31 @@
+import Image from "next/image";
+import angelLogo from "../app/assets/angel-logo.png";
+import devilLogo from "../app/assets/devil-logo.png";
+import userLogo from "../app/assets/user-logo.png";
+
 export default function Message({
   message,
   isBot,
+  isHeaven,
 }: {
   message: String;
   isBot: Boolean;
+  isHeaven: Boolean;
 }) {
   return (
-    <div id="lst-msg" className="flex justify-start mr-8 gap-2">
+    <div
+      id="lst-msg"
+      className={`flex ${
+        isBot ? "justify-right flex-row-reverse": "justify-start mr-8"
+      } gap-2`}
+    >
       <div className="border border-zinc-300 h-5 w-5 bg-white rounded-full overflow-hidden -mt-1">
-        <img alt="profile picture" />
+        <Image
+          width={20}
+          height={20}
+          alt="profile picture"
+          src={isBot ? userLogo : isHeaven ? angelLogo : devilLogo}
+        />
       </div>
       <div
         className="max-w-[calc(100%-50px)] mb-3 rounded-b-lg rounded-tr-lg py-3 px-4"

c6be437c6c78009bf1fd90016078046aa9c4dcdf
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-01 20:17:06 +0530

    Added create API for message

--- src/app/api/v1/message/route.ts
+++ src/app/api/v1/message/route.ts
@@ -4,19 +4,51 @@ import Message from "@/model/message.model";
 
 connect();
 
-export async function GET(request:NextRequest) {
-    try {
-        const messages = await Message.find({});
-        return NextResponse.json({
-            hell: messages.filter(ele => !ele.isHeaven),
-            heaven: messages.filter(ele => ele.isHeaven),
-        })
-    } catch (err: unknown) {
-        console.log(err);
-        NextResponse.json({
-            error: "Error on fetching data",
-        }, {
-            status: 401
-        })
-    }
+export async function GET(request: NextRequest) {
+  try {
+    const messages = await Message.find({});
+    return NextResponse.json({
+      hell: messages.filter((ele) => !ele.isHeaven),
+      heaven: messages.filter((ele) => ele.isHeaven),
+      success: true,
+    });
+  } catch (err: unknown) {
+    console.log(err);
+    NextResponse.json(
+      {
+        error: "Error on fetching data",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { message, isBot, isHeaven } = reqBody;
+    const newMessage = new Message({
+      message,
+      isBot,
+      isHeaven,
+    });
+    await newMessage.save();
+    return NextResponse.json({
+      message: "Message added succesfully",
+      success: true,
+    });
+    return NextResponse.json({});
+  } catch (err: unknown) {
+    console.log(err);
+    NextResponse.json(
+      {
+        error: "Error on adding data",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }

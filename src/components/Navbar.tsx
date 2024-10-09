070611c77e89ef6bc25fa9ba6e067251d00ef5c1
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-09 20:44:05 +0530

    added link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import logo from "../app/assets/hh-logo.png";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { ChevronDown, LogIn, LogOut } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar() {
   const session = useSession();
@@ -11,7 +12,7 @@ export default function Navbar() {
     <nav className="absolute top-0 flex justify-between left-0 w-[100vw] px-3 py-2 bg-black z-20 text-white shadow-lg border-b border-white">
       <div className="flex items-center gap-3">
         {/* <Image src={logo} width={30} height={30} alt="log" /> */}
-        <h1 className="text-xl font-semibold mb-1">Hell Heaven AI</h1>
+        <Link href="/" className="text-xl font-semibold mb-1">Hell Heaven AI</Link>
       </div>
       <div className="flex items-center mr-1">
         {session.status === "authenticated" ? (

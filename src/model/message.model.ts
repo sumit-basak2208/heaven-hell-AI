3b60e61d4ce0e1778fb78e0681b3458e10f07d41
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-13 18:29:26 +0530

    added timestamp

--- src/model/message.model.ts
+++ src/model/message.model.ts
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-  message: String,
-  isBot: Boolean,
-  isHeaven: Boolean,
-  email: String
-});
+const messageSchema = new mongoose.Schema(
+  {
+    message: String,
+    isBot: Boolean,
+    isHeaven: Boolean,
+    email: String,
+  },
+  { timestamps: true }
+);
 
 const Message =
   mongoose.models.messages || mongoose.model("messages", messageSchema);
